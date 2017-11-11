fe26babb054283e769c60761fd273086d8326564
puppy <971366114@qq.com>
2017-11-11 17:19:32 +0800

    - redis 存储类

--- src/Storage/RedisStorage.php
+++ src/Storage/RedisStorage.php
@@ -2,5 +2,11 @@
 namespace Lhlog\Storage;
 // use 
 class RedisStorage extends Base{
-    
+    // redis 对象
+    public $redis;
+
+    public function init( array $config = [] ){
+        parent::init( $config );
+        
+    }
 } 
